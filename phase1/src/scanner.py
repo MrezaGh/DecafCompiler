8837bc2741ec7a991e71c37b4c65205d8b848835
Mreza Gh <mrezagh.legend@gmail.com>
2020-10-23 23:50:37 +0330

    :bug: Fix the error with especial IDs

--- phase1/src/scanner.py
+++ phase1/src/scanner.py
@@ -5,11 +5,12 @@ import lark
 #     1. Add the keys needed for scanner using Docs
 #     2. add regex for distinguishing tokens
 grammar = r"""
-start: (T_BOOLEANLITERAL | T_KEYWORD | T_ID | T_INTLITERAL | T_DOUBLELITERAL | T_STRINGLITERAL | T_COMMENT | T_OPR | T_OTHERS | UNDEFINED_TOKEN ) start |
-T_SPEC_ID.3: (T_KEYWORD /[0-9]+/)| T_BOOLEANLITERAL /[0-9]+/
+start: (declaration)+
+declaration : T_BOOLEANLITERAL | T_KEYWORD | T_INTLITERAL | T_DOUBLELITERAL | T_STRINGLITERAL | T_COMMENT | T_OPR | T_OTHERS | UNDEFINED_TOKEN | T_ID | T_SPEC_ID | T_INTERFACE
+T_SPEC_ID.3: (T_KEYWORD (/[0-9]+/ | CNAME))| T_BOOLEANLITERAL (/[0-9]+/ | CNAME)
 T_BOOLEANLITERAL.2: ("true" | "false")
 T_KEYWORD.2: ("void" | "interface" | "int" | "double" | "bool" | "string" | "class" | "null" | "this" | "extends" | "implements" | "for" | "while" | "if" | "else" | "return" | "break" | "continue" | "new" | "NewArray" | "Print" | "ReadInteger" | "ReadLine" | "dtoi" | "itod" | "btoi" | "itob" | "private" | "protected" | "public")
-T_ID: CNAME | T_SPEC_ID
+T_ID: CNAME
 T_INTLITERAL.2: /0[xX][0-9a-fA-F]+/ | /[0-9]+/
 T_DOUBLELITERAL.2:  /[0-9]+[\.][0-9]*[eE][+ | -]?[0-9]+/ | /[0-9]+\.[0-9]*/
 T_STRINGLITERAL: /"(?:[^\\"]|\\.)*"/
@@ -17,6 +18,7 @@ T_COMMENT: /\/\/.*/
 T_MULTILINE_COMMENT : /\/\*(\*(?!\/)|[^*])*\*\//
 T_OPR: /[<>!=]=/ | /[+*-=()%<>;:!,]/ | "||" | "]" | "[" | "." | "/" | "&&"
 T_OTHERS: "(" | ")" | "{" | "}"
+T_INTERFACE.4: "interface"
 UNDEFINED_TOKEN: (T_INTLITERAL CNAME) | (T_DOUBLELITERAL CNAME)
 %import common.WORD   // imports from terminal library
 %import common.CNAME
@@ -30,7 +32,28 @@ code = """
 Hello, World!
 """
 code = """
-int inta aint ainta int2 2int 2inta 2aint
+void
+int
+double
+bool
+string
+class
+interface
+null
+this
+extends
+implements
+for
+while
+if
+else
+return
+break
+new
+NewArray
+Print
+ReadInteger
+ReadLine
 """
 
 
@@ -55,8 +78,12 @@ def pretty_print(tree: lark.Tree):
             if token.type == "T_OPR" or token.type == "T_KEYWORD" or token.type == "T_OTHERS":
                 # print(token)
                 tokens.append(str(token))
-            elif token.type == "T_COMMENT":
+            elif token.type == "T_COMMENT" or token.type == "T_MULTILINE_COMMENT":
                 pass
+            elif token.type == "T_SPEC_ID":
+                tokens.append("T_ID" + " " + str(token))
+            elif token.type == "T_INTERFACE":
+                tokens.append(str(token))
             else:
                 # print(token.type, token)
                 tokens.append(str(token.type) + " " + str(token))
