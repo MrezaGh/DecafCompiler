d54a5b645a1b46e0eb0d437512716499b84c90fc
Mreza Gh <mrezagh.legend@gmail.com>
2020-10-23 23:03:06 +0330

    :bug: debug due to test fails

--- phase1/src/scanner.py
+++ phase1/src/scanner.py
@@ -5,17 +5,18 @@ import lark
 #     1. Add the keys needed for scanner using Docs
 #     2. add regex for distinguishing tokens
 grammar = r"""
-start: (T_BOOLEANLITERAL | T_KEYWORD | T_ID | T_INTLITERAL | T_DOUBLELITERAL | T_STRINGLITERAL | T_COMMENT | T_OPR | UNDEFINED_TOKEN ) start |
+start: (T_BOOLEANLITERAL | T_KEYWORD | T_ID | T_INTLITERAL | T_DOUBLELITERAL | T_STRINGLITERAL | T_COMMENT | T_OPR | T_OTHERS | UNDEFINED_TOKEN ) start |
 T_SPEC_ID.3: (T_KEYWORD /[0-9]+/)| T_BOOLEANLITERAL /[0-9]+/
 T_BOOLEANLITERAL.2: ("true" | "false")
-T_KEYWORD.2: ("void" | "int" | "bool" | "string" | "class" | "interface" | "null" | "this" | "extends" | "implements" | "for" | "while" | "if" | "else" | "return" | "break" | "continue" | "new" | "NewArray" | "Print" | "ReadInteger" | "ReadLine" | "dtoi" | "itod" | "btoi" | "itob" | "private" | "protected" | "public")
+T_KEYWORD.2: ("void" | "interface" | "int" | "double" | "bool" | "string" | "class" | "null" | "this" | "extends" | "implements" | "for" | "while" | "if" | "else" | "return" | "break" | "continue" | "new" | "NewArray" | "Print" | "ReadInteger" | "ReadLine" | "dtoi" | "itod" | "btoi" | "itob" | "private" | "protected" | "public")
 T_ID: CNAME | T_SPEC_ID
-T_INTLITERAL: /0[xX][0-9a-fA-F]+/ | /[0-9]+/
-T_DOUBLELITERAL:  /[0-9]*[\.][0-9]+[eE][+ | -][0-9]+/ | /[0-9]+\.[0-9]*/
+T_INTLITERAL.2: /0[xX][0-9a-fA-F]+/ | /[0-9]+/
+T_DOUBLELITERAL.2:  /[0-9]+[\.][0-9]*[eE][+ | -]?[0-9]+/ | /[0-9]+\.[0-9]*/
 T_STRINGLITERAL: /"(?:[^\\"]|\\.)*"/
 T_COMMENT: /\/\/.*/
 T_OPR: /[<>!=]=/ | /[+*-=()%<>;:!,]/ | "||" | "]" | "[" | "." | "/" | "&&"
-UNDEFINED_TOKEN.4: (T_INTLITERAL CNAME) | (T_DOUBLELITERAL CNAME)
+T_OTHERS: "(" | ")" | "{" | "}"
+UNDEFINED_TOKEN: (T_INTLITERAL CNAME) | (T_DOUBLELITERAL CNAME)
 %import common.WORD   // imports from terminal library
 %import common.CNAME
 %import common.WS
@@ -26,28 +27,7 @@ code = """
 Hello, World!
 """
 code = """
-void
-int
-double
-bool
-string
-class
-interface
-null
-this
-extends
-implements
-for
-while
-if
-else
-return
-break
-new
-NewArray
-Print
-ReadInteger
-ReadLine
+int inta aint ainta int2 2int 2inta 2aint
 """
 
 
@@ -69,7 +49,7 @@ def pretty_print(tree: lark.Tree):
             # pretty_print(token)
             tokens.extend(pretty_print(token))
         else:
-            if token.type == "T_OPR" or token.type == "T_KEYWORD":
+            if token.type == "T_OPR" or token.type == "T_KEYWORD" or token.type == "T_OTHERS":
                 # print(token)
                 tokens.append(str(token))
             elif token.type == "T_COMMENT":
